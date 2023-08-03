39e1b626ed82f95f03698b7214289ede72707d44
Abiela Sarrieddine <asarrieddine23@gmail.com>
2023-08-03 11:42:48 -0700

    Added info button for flask

--- STEM Playground/Lab.swift
+++ STEM Playground/Lab.swift
@@ -8,18 +8,39 @@
 import SwiftUI
 
 struct Lab: View {
+    @State private var flaskInfo1 = ("Flasks are used for making, storing, or measuring solutions and samples!")
+    @State private var flaskInfo2 = ("Flasks")
+    @State private var placeholderFlask = ("")
     var body: some View {
-        ZStack{
-            Image("labscene")
-                .resizable(resizingMode: .stretch)
-                .aspectRatio(contentMode: .fill)
+        NavigationStack{
+            
+            ZStack{
+               
+                Image("labscene")
+                 .resizable(resizingMode: .stretch)
+                 .aspectRatio(contentMode: .fill)
                 .ignoresSafeArea()
+                Text("Flask!")
+                    .padding(.top, 220.0)
+                    .frame(height: nil)
+                Button("Flask!") {
+                    placeholderFlask = "\(flaskInfo1)"
+                }
+                .padding(.top, 220.0)
+                Text(placeholderFlask)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.blue)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 320.0)
+                    .frame(width: 175.0)
+                
+            }
         }
     }
-}
-
-struct Lab_Previews: PreviewProvider {
-    static var previews: some View {
-        Lab()
+    
+    struct Lab_Previews: PreviewProvider {
+        static var previews: some View {
+            Lab()
+        }
     }
 }
