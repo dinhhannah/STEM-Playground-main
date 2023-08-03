6e67ffc788aae823ce26ca6e41b6726c43846296
Abiela Sarrieddine <asarrieddine23@gmail.com>
2023-08-03 13:37:11 -0700

    lava lamp in progress

---
+++ lavalamp1.swift
@@ -0,0 +1,27 @@
+//
+//  lavalamp1.swift
+//  STEM Playground
+//
+//  Created by Camacho Clan on 8/3/23.
+//
+
+import SwiftUI
+
+struct lavalamp1: View {
+    var body: some View {
+        NavigationStack {
+            Text("Step 1:")
+                .fontWeight(.bold)
+            Text(" \n Fill the bottle about halfway with water and add a few drops of food coloring")
+                .multilineTextAlignment(.center)
+            Image("lavalampstep1")
+            
+        }
+    }
+    
+    struct lavalamp1_Previews: PreviewProvider {
+        static var previews: some View {
+            lavalamp1()
+        }
+    }
+}
