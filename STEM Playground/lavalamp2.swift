9fc1705f3ff3fd87f630f362f911ca59998aada5
Abiela Sarrieddine <asarrieddine23@gmail.com>
2023-08-03 14:05:56 -0700

    lava lamp so far

---
+++ STEM Playground/lavalamp2.swift
@@ -0,0 +1,20 @@
+//
+//  lavalamp2.swift
+//  STEM Playground
+//
+//  Created by Camacho Clan on 8/3/23.
+//
+
+import SwiftUI
+
+struct lavalamp2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct lavalamp2_Previews: PreviewProvider {
+    static var previews: some View {
+        lavalamp2()
+    }
+}
