8013b45aa036253909564132d1db85c267cf2647
shreya <shreya.arvnd@gmail.com>
2023-08-03 13:59:40 -0700

    minor edits - grammar

--- STEM Playground/City.swift
+++ STEM Playground/City.swift
@@ -20,7 +20,7 @@ struct City: View {
             
             Button("Car") {
                 box = "whitebox"
-                info = "Vehicle pollutants are bad for our health and contain greenhouse gases that lead to climate change. When we burn gasoline and diesel fuel in vehicles, harmful byproducts like nitrogen dioxide and carbon monoxide are produced. Additionally, vehicles release carbon dioxide, which is the most common human-caused greenhouse gas."
+                info = "Vehicle pollutants are bad for our health and contain greenhouse gases that lead to climate change. When we burn gasoline and diesel fuel in vehicles, harmful byproducts like nitrogen dioxide and carbon monoxide are produced."
                 close = "X"
             }
             .padding(.top, 630)
@@ -32,10 +32,10 @@ struct City: View {
                 info = "UV rays are a type of invisible radiation with a shorter wavelength than visible light. The sun is the most powerful source of UV radiation around us. However, prolonged exposure to UV rays can increase the risk of skin cancer for people. Therefore, it's important to protect ourselves from excessive UV exposure, especially when spending time outdoors in the sun. Using sunscreen, wearing protective clothing, and seeking shade can help reduce this risk and keep our skin healthy."
                 close = "X"
             }
-            .padding(.bottom, 740.0)
-            .padding(.trailing, 280.0)
+            .padding(.bottom, 700)
+            .padding(.trailing, 200)
             
-            Button("Pollution and Spmog") {
+            Button("Pollution and Smog") {
                 box = "whitebox"
                 info = "In factories, harmful materials and gases, such as carbon dioxide and methane, are burned and released into the air. These gases absorb sunlight and directly impact the planet's temperature."
                 close = "X"
