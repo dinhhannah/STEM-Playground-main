22797ea7f91c040f7678011778200d2e488f1932
shreya <shreya.arvnd@gmail.com>
2023-08-03 13:45:31 -0700

    added kitchen view

---
+++ STEM Playground/Kitchen.swift
@@ -0,0 +1,20 @@
+//
+//  Kitchen.swift
+//  STEM Playground
+//
+//  Created by Shreya Aravind on 8/3/23.
+//
+
+import SwiftUI
+
+struct Kitchen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Kitchen_Previews: PreviewProvider {
+    static var previews: some View {
+        Kitchen()
+    }
+}
