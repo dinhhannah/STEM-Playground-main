d7feeb8eac0e9391e91a0842ee0ac61a018bcb20
Hannah Dinh <hannahdinhHD@gamil.com>
2023-08-03 12:53:09 -0700

    buttons!!!! city and lab

--- STEM Playground/City.swift
+++ STEM Playground/City.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct City: View {
+    @State private var box = ""
+    @State private var info = ""
+    @State private var close = ""
+    
     var body: some View {
         ZStack{
             Image("cityscene")
                 .resizable(resizingMode: .stretch)
                 .ignoresSafeArea()
+            
+            Button("Car") {
+                box = "whitebox"
+                info = "i love to play bumper cars"
+                close = "X"
+            }
+            .padding(.top, 630)
+            .padding(.leading, 110)
+            
+            
+            Button("Sun") {
+                box = "whitebox"
+                info = "it's safe to stare at trust me"
+                close = "X"
+            }
+            .padding(.bottom, 740.0)
+            .padding(.trailing, 280.0)
+            
+            Button("Pollution and smog") {
+                box = "whitebox"
+                info = "not cool thumbs down"
+                close = "X"
+            }
+            .padding(.bottom, 400)
+            .padding(.leading, 250.0)
+            
+            Image(box)
+            Text(info)
+                .multilineTextAlignment(.center)
+                .padding(.trailing, 100)
+                .padding(.leading, 100)
+
+            Button(close) {
+                box = ""
+                info = ""
+                close = ""
+            }
+            .padding(.bottom, 190.0)
+            .padding(.trailing, 190.0)
+            .foregroundColor(Color.red)
             
             HStack{
                 NavigationLink(destination: Lab()){
--- STEM Playground/Lab.swift
+++ STEM Playground/Lab.swift
@@ -8,10 +8,9 @@
 import SwiftUI
 
 struct Lab: View {
-    
-    @State private var flaskInfo1 = ("Flasks are used for making, storing, or measuring solutions and samples!")
-    @State private var flaskInfo2 = ("Flasks")
-    @State private var placeholderFlask = ("")
+    @State private var box = ""
+    @State private var info = ""
+    @State private var close = ""
     
     var body: some View {
         NavigationStack{
@@ -21,23 +20,35 @@ struct Lab: View {
                     .aspectRatio(contentMode: .fill)
                     .ignoresSafeArea()
                 
-                Text("Flask!")
-                    .padding(.top, 220.0)
-                    .frame(height: nil)
-                
-                Button("Flask!") {
-                    placeholderFlask = "\(flaskInfo1)"
+                Button("Flasks") {
+                    box = "whitebox"
+                    info = "Flasks are used for making, storing, or measuring solutions and samples!"
+                    close = "X"
                 }
-                .padding(.top, 220.0)
+                .padding(.top, 230.0)
                 
-                Text(placeholderFlask)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.blue)
+                Button("Microscope") {
+                    box = "whitebox"
+                    info = "Microscopes are used for seeing things up close!"
+                    close = "X"
+                }
+                .padding(.top, 230.0)
+                .padding(.trailing, 260.0)
+                
+                Image(box)
+                Text(info)
                     .multilineTextAlignment(.center)
-                    .padding(.top, 320.0)
-                    .frame(width: 175.0)
-                
-//                Img()
+                    .padding(.trailing, 100)
+                    .padding(.leading, 100)
+
+                Button(close) {
+                    box = ""
+                    info = ""
+                    close = ""
+                }
+                .padding(.bottom, 190.0)
+                .padding(.trailing, 190.0)
+                .foregroundColor(Color.red)
                 
                 HStack{
                     Spacer()
@@ -51,6 +62,7 @@ struct Lab: View {
                     
                 }
             }
+            .padding(.horizontal)
         }
     }
 }
