8db636abc0d535a9bc4e8bdaecdb00743e746301
Alena Peethala <alenadashi@icloud.com>
2023-08-03 20:03:10 -0700

    Added the rocket experiment screens (DID NOT add it to the DIY home page yet)

---
+++ STEM Playground/rocket4.swift
@@ -0,0 +1,39 @@
+//
+//  rocket4.swift
+//  STEM Playground
+//
+//  Created by Alena Peethala on 8/3/23.
+//
+
+import SwiftUI
+
+struct rocket4: View {
+    var body: some View {
+            NavigationStack {
+                Text("Step 4")
+                    .fontWeight(.bold)
+                
+                Image("rocketexp4")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+               
+                Text(" \n Insert a wine cork into the mouth of the pop bottle, make sure it is in there tightly. Now take it outside with open space! You need to move quickly for this next part. Flip the bottle over, and QUICKLY place it in the launchpad!  Hurry back as the countdown begins as soon as the baking soda and vinegar touch." )
+                    .multilineTextAlignment(.center)
+                
+                
+                 NavigationLink(destination: DIYhomepage ()) {
+                 Text("Try another experiment!")
+                 .fontWeight(.bold)
+                  .foregroundColor(Color.blue)
+                 }
+                
+            }
+        }
+        
+        struct rocket4_Previews: PreviewProvider {
+            static var previews: some View {
+                rocket4()
+            }
+        }
+    }
+
