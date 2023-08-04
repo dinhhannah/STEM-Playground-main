dca8a7132d57f80134f090afe7a2f6772aceaca9
Alena Peethala <alenadashi@icloud.com>
2023-08-04 10:28:50 -0700

    added the oobleck page

---
+++ STEM Playground/oobleck.swift
@@ -0,0 +1,20 @@
+//
+//  oobleck.swift
+//  STEM Playground
+//
+//  Created by Alena Peethala on 8/4/23.
+//
+
+import SwiftUI
+
+struct oobleck: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct oobleck_Previews: PreviewProvider {
+    static var previews: some View {
+        oobleck()
+    }
+}
